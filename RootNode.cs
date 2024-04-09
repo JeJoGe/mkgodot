117a042a1cf1805e56cc4d37d2914d36eddd1b7a
Jessica Lee <jessicalee@Jessicas-MacBook-Pro.local>
2024-04-08 19:10:04 -0700

    CharacterChoice Scene - inprogress

--- RootNode.cs
+++ RootNode.cs
@@ -3,7 +3,12 @@ using System;
 
 public partial class RootNode : Node2D
 {
-    public RootNode() {
-        GD.Print("Hello world!");
-    }
+	public RootNode() {
+		GD.Print("Hello world!");
+	}
+
+	public override void _Ready() {
+		var CharacterChoiceScene = ResourceLoader.Load<PackedScene>("res://CharacterChoiceScene/character_choice.tscn").Instantiate();
+		AddChild(CharacterChoiceScene);
+	}
 }
