f6208621ee4ce3c6ba350be0a0263030f19a01d4
getam <Festian.Pierrot@gmail.com>
2024-04-29 19:43:30 -0700

    debugging local coords. y off by -1

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,22 +3,29 @@ using System;
 
 public partial class Player : Node2D
 {
+	const int MainLayer = 0;
 	public override void _Ready()
 	{	
-		var mapGen = GetNode<TileMap>("../MapGen");
+		var mapGen = GetNode<MapGen>("../MapGen");
 		//GD.Print(mapGen.ToGlobal(mapGen.MapToLocal(new Vector2I(0,0))));
 		GlobalPosition = mapGen.ToGlobal(mapGen.MapToLocal(new Vector2I(0,0)));
 	}
 	public override void _Input(InputEvent @event)
     {
-		//if (@event is InputEventMouseButton eventMouseButton)
-			//if (eventMouseButton.Pressed && eventMouseButton.ButtonIndex == MouseButton.Left)
-			//{
-				//var globalClicked = eventMouseButton.Position;
-				//var posClicked = LocalToMap(ToLocal(globalClicked));
-				//GD.Print("TileMap: " + posClicked.ToString());
-				//var currentAtlasCoords = GetCellAtlasCoords(MainLayer, posClicked);
-				//GD.Print("Atlas: " + currentAtlasCoords.ToString());
-			//}
+		var mapGen = GetNode<MapGen>("../MapGen");
+		// Called on Input Event. For now, should only process mouse event Leftclick
+		if (@event is InputEventMouseButton eventMouseButton)
+			if (eventMouseButton.Pressed && eventMouseButton.ButtonIndex == MouseButton.Left)
+			{
+				// Converting global pixel coordinates to coordinates on the MapGen node then converting to the Hex coordinates of MapGen
+				var globalClicked = GetGlobalMousePosition();    
+				var posClicked = mapGen.LocalToMap(mapGen.ToLocal(globalClicked));
+				GD.Print("TileMap: " + posClicked.ToString());
+				// Atlas coordinates are the tile's coordinates on the atlas the tilemap is pulling tiles from
+				var currentAtlasCoords = mapGen.GetCellAtlasCoords(MainLayer, posClicked);
+				GD.Print("Atlas: " + currentAtlasCoords.ToString());
+
+				mapGen.generateTile(currentAtlasCoords, posClicked);
+			}
 	}
 }
