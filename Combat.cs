a660d73d64db2a79569b92e57a49fd18414f0b88
getam <Festian.Pierrot@gmail.com>
2024-06-25 22:41:24 -0700

    Fixed signals + node paths

--- Combat.cs
+++ Combat.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public partial class Combat : Control
+public partial class Combat : Node2D
 {
 	[Export]
 	private ConfirmationDialog _confirmDialog;
