6b5d1ffd82569feafbfe6957a186c7dda2e122a8
Getam <geran.tam@gmail.com>
2024-10-07 16:09:16 -0700

    testing code

--- MapToken.cs
+++ MapToken.cs
@@ -80,7 +80,6 @@ public partial class MapToken : Node2D
 						{
 							monsterGroup.MonsterList.Add(gameplayControl.PlaceholderMonsterGen(monsterColour, 0, MapPosition));
 						}
-						
 						monsterGroup.GlobalPosition = mapGen.ToGlobal(mapGen.MapToLocal(MapPosition));
 						AddChild(monsterGroup);
 						gameplayControl.MonsterGroupList.Add(monsterGroup);
