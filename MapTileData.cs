99fe2c8f5e3d1ab24cf06ef13a2517595b96003e
getam <Festian.Pierrot@gmail.com>
2024-10-22 16:52:00 -0700

    small changes

--- MapTileData.cs
+++ MapTileData.cs
@@ -1,9 +1,12 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 public partial class MapTileData : Node2D
 {
+    int cellTerrain;
+    
     public MapTileData(int cellTerrain, TileData tileData)
     {
 
