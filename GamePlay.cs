874331915056031d447927bcdd3a4c7c5edec06c
Jessica Lee <34080714+jessicachakmlee@users.noreply.github.com>
2024-12-17 22:11:29 -0800

    Jess: 12/9 - add wound to discard (#72)

--- GamePlay.cs
+++ GamePlay.cs
@@ -73,7 +73,7 @@ public partial class GamePlay : Node2D
 				{
 					quantity = Convert.ToInt16(action[3]);
 				}
-				else if( action[0] != nameof(BasicCardActions.gainManaTokens))
+				else if (action[0] != nameof(BasicCardActions.gainManaTokens))
 				{
 					quantity = Convert.ToInt16(action[1]);
 				}
@@ -206,7 +206,8 @@ public partial class GamePlay : Node2D
 			}
 		}
 
-		if(specialAction != null) {
+		if (specialAction != null)
+		{
 
 		}
 	}
